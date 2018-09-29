550da2c0163f9dbdb12a584504898b4d1e294a23
Pawel <pawelsafian@gmail.com>
2018-09-29 14:50:05 +0200

    Zadanie 2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var menuButton = document.querySelector('.for-dropdown');
-  console.log(menuButton);
 
   var subMenu = document.querySelector('.dropdown')
 
@@ -12,4 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
   menuButton.addEventListener('mouseout', function() {
     subMenu.style.display = 'none';
   });
+
+  var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons[0].previousElementSibling);
+
+  function showHide () {
+    var textArea = this.previousElementSibling;
+
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+}
+
+  // buttons[0].addEventListener('click', showHide);
+
+  for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', showHide);
+  }
 });
